cce2ea198ffd188a4a0ac0ca60331aa925680d74
hugolara31 <hugolara31>
2023-11-07 01:14:00 -0300

    agregue ejemplo

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-function miCallback() {
+function suma (a,b,callback){
+    let c = a +b;
+    callback()
+}
+
+function Callback() {
     console.log("La suma se ha calculado.");
   }
   
-  suma(2, 3, miCallback);
+  suma(2, 3, Callback);
